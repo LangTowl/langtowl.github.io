5f79fe2b4104e32e1a6205c08cc792465a65e8e4
Lang <119524847+LangTowl@users.noreply.github.com>
2024-05-23 13:07:02 -0400

    Add files via upload

--- scripts/file_manager.js
+++ scripts/file_manager.js
@@ -1,3 +1,4 @@
+// FILE MANAGER START
 var button = document.getElementById("folder_button_about_me_listener");
 var image = document.getElementById("about_me_chevron");
 
@@ -17,16 +18,19 @@ document.getElementById('main_content').innerHTML = data;
 .catch(error => console.error('Error fetching content:', error));
 
 button.addEventListener("click", function() {
-    console.log("About Me Button Pressed");
 
     if (current_image_path == image_path_1) {
         image.src = image_path_2;
         current_image_path = image_path_2;
         current_html_path = html_path_1;
+
+        console.log("About Me Button Pressed | chevron down -> chevron right");
     } else {
         image.src = image_path_1;
         current_image_path = image_path_1;
         current_html_path = html_path_2;
+
+        console.log("About Me Button Pressed | chevron right -> chevron down");
     }
 
     fetch(current_html_path)
@@ -36,3 +40,4 @@ button.addEventListener("click", function() {
     })
     .catch(error => console.error('Error fetching content:', error));
 })
+// FILE MANAGER END
--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,25 @@ button.addEventListener("click", function() {
 
     var grid_container = document.querySelector(".grid_container");
     var toggle_folders = document.querySelector(".folder");
+    var toggle_folder_contents = document.querySelector(".folder_contents")
+
+    var image = document.getElementById("about_me_chevron");
+    var image_path_2 = "assets/svg/chevron right icon.svg";
     
+
     if (is_expanded) {
         grid_container.style.gridTemplateColumns = "10% 90%";
         toggle_folders.style.display = "none";
+        toggle_folder_contents.style.display = "none";
+        image.src = image_path_2;
+
+        console.log(current_image_path);
         console.log("Shrink file manager");
     } else {
         grid_container.style.gridTemplateColumns = "30% 70%";
         toggle_folders.style.display = "flex";
+
+
         console.log("Expand file manager");
     }
 
