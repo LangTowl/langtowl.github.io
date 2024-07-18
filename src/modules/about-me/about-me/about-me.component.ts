a4214951bb69af39804aca43b858107416b86c36
Lang Towl <langtowl@gmail.com>
2024-07-18 14:47:56 -0400

    Updates persistence of nav bar and update navbar width logic

--- src/modules/about-me/about-me/about-me.component.ts
+++ src/modules/about-me/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewControllerService } from '../../../services/view-controler/view-controller.service';
 
 @Component({
   selector: 'app-about-me',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AboutMeComponent {
 
+  constructor(
+    private view_controller_service: ViewControllerService
+  ) { }
+
+  ngOnInit() {
+    // Ensures that if reload happens on non home screen, nav bar is showing
+    this.view_controller_service.updateNavBarState(true);
+  }
 }
