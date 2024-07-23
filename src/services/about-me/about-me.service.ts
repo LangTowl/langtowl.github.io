27cc5c46caa05878e7bdb5ab5b1d377069db64aa
Lang Towl <langtowl@gmail.com>
2024-07-22 20:49:03 -0400

    Fixes line wrap issue for about me content, adds blur background for nav buttons, adds margin to bottom of content for scolability.

--- src/services/about-me/about-me.service.ts
+++ src/services/about-me/about-me.service.ts
@@ -13,21 +13,19 @@ export class AboutMeService {
   private about_me_posts: AboutMePost[] = [
     {
       title: 'Langs Bio',
-      content: `I grew up surfing year-round in the frigid waters of Maine and New England. Living on this pristine coast fostered a deep respect for nature and the marine ecosystems on which our modern world depends. This connection to the ocean inspired me to pursue a STEM education to one day apply these skills to initiatives focused on environmental preservation. 
-      
-      While I still care deeply about the ocean, my interests have evolved into different STEM applications. I have always been fascinated by difficult technical problems and the bright minds who solve them. Few industries have to solve more complex problems than the aerospace and defense industry. I aspire to work within a team of engineers tackling such problems, allowing me to grow my skills in software development, hardware design, and sustainability.`
+      content: `I grew up surfing year-round in the frigid waters of Maine and New England. Living on this pristine coast fostered a deep respect for nature and the marine ecosystems on which our modern world depends. This connection to the ocean inspired me to pursue a STEM education to one day apply these skills to initiatives focused on environmental preservation. \n\nWhile I still care deeply about the ocean, my interests have evolved into different STEM applications. I have always been fascinated by difficult technical problems and the bright minds who solve them. Few industries have to solve more complex problems than the aerospace and defense industry. I aspire to work within a team of engineers tackling such problems, allowing me to grow my skills in software development, hardware design, and sustainability.`
     },
     {
       title: 'Career Objective',
-      content: 'Career Objective Placeholder'
+      content: 'I am a junior studying computer engineering concentrated in machine intelligence. I am currently pursuing a summer 2024 internship in the aerospace and defense industry where I can apply my skills in software development, hardware design, and sustainability.'
     },
     {
       title: 'Work Experience',
-      content: 'Work Experience Placeholder'
+      content: 'Work experience needs work haha'
     },
     {
       title: 'Education',
-      content: 'Education Placeholder'
+      content: 'I am currently a first semester senior studying computer engineering at Florida Polytechnic University. This ABET accredited degree is concentrated in machine intelligence. Topics in this concentration include but are not limited to: digital image processing, neural networks and applications, mixed hardware-software systems, and machine learning hardware.'
     }
   ];
   
